2e80f9dccd82598dbb586b357f1cf8f867fb6d2a
com-morishita <tm.morishita@ntt.com>
2023-10-31 18:38:13 +0900

    Replace Header

--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -3,7 +3,6 @@ import {
 } from 'react-native'
 import { router } from 'expo-router'
 
-import Header from '../../components/Header'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 
@@ -14,7 +13,6 @@ const handlePress = (): void => {
 const Edit = (): JSX.Element => {
   return (
     <KeyboardAvoidingView behavior='height' style={styles.container}>
-      <Header />
 
       <View style={styles.inputContainer}>
         <TextInput multiline style={styles.input} value={'買い物\nリスト'} />
