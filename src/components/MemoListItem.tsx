dc927db8265691ffc404e7a8b1c3387eaa1cd8f4
com-morishita <tm.morishita@ntt.com>
2023-10-31 17:55:00 +0900

    Apply TouchableOpacity

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import Icon from './Icon'
 
 const MemoListItem = (): JSX.Element => {
   return (
@@ -7,9 +8,9 @@ const MemoListItem = (): JSX.Element => {
         <Text style={styles.memoListItemTitle}>買い物リスト</Text>
         <Text style={styles.memoListItemDate}>2023/10/1 10:00</Text>
       </View>
-      <View>
-        <Text>X</Text>
-      </View>
+      <TouchableOpacity>
+        <Icon name='delete' size={32} color='#b0b0b0' />
+      </TouchableOpacity>
     </View>
   )
 }
