1af57de6ff5053b22b8f64cf0b2de8abab188bde
com-morishita <tm.morishita@ntt.com>
2023-11-10 20:33:12 +0900

    Implement log in

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -51,7 +51,7 @@ const SignUp = (): JSX.Element => {
         <Button label='Submit' onPress={() => { handlePress(email, password) }} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registered?</Text>
-          <Link href='/auth/log_in' asChild>
+          <Link href='/auth/log_in' asChild replace>
             <TouchableOpacity>
               <Text style={styles.footerLink}>Log In.</Text>
             </TouchableOpacity>
