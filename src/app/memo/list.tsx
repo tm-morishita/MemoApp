e8d0bbfc3485506adbbd33cf43f543d17cb0bbf1
com-morishita <tm.morishita@ntt.com>
2023-11-15 18:16:16 +0900

    Implement memolist display

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,11 +1,14 @@
 import { View, StyleSheet } from 'react-native'
 import { router, useNavigation } from 'expo-router'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import LogOutButton from '../../components/LogOutButton'
+import { db, auth } from '../../config'
+import { type Memo } from '../../../types/memo'
 
 const handlePress = (): void => {
   router.push('/memo/create')
@@ -13,6 +16,7 @@ const handlePress = (): void => {
 
 const List = (): JSX.Element => {
   const navigation = useNavigation()
+  const [memos, setMemos] = useState<Memo[]>([])
 
   useEffect(() => {
     navigation.setOptions({
@@ -20,12 +24,30 @@ const List = (): JSX.Element => {
     })
   }, [])
 
+  useEffect(() => {
+    if (auth.currentUser === null) return
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    const q = query(ref, orderBy('updatedAt', 'desc'))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const remoteMemos: Memo[] = []
+      snapshot.forEach((doc) => {
+        console.log('memo', doc.data())
+        const { bodyText, updatedAt } = doc.data()
+        remoteMemos.push({
+          id: doc.id,
+          bodyText,
+          updatedAt
+        })
+      })
+      setMemos(remoteMemos)
+    })
+    return unsubscribe
+  }, [])
+
   return (
     <View style={styles.container}>
       <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
+        {memos.map((memo) => <MemoListItem memo={memo} />)}
       </View>
       <CircleButton onPress={handlePress}>
         <Icon name='plus' size={40} color='#ffffff' />
